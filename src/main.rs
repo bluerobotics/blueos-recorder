77750cddb6d008978c4d6d9b58cf5dc138694cf2
Patrick José Pereira <patrickelectric@gmail.com>
2025-07-09 16:10:03 -0300

    src: Integrate with cli
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
+mod cli;
 mod service;
 use service::Service;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    env_logger::init();
+    cli::init();
+    if cli::is_verbose() {
+        env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("debug")).init();
+    } else {
+        env_logger::init();
+    }
 
     let mut config = zenoh::Config::default();
     config
@@ -19,7 +25,18 @@ async fn main() -> anyhow::Result<()> {
         .insert_json5("metadata", r#"{"name": "blueos-recorder"}"#)
         .expect("Failed to insert metadata");
 
-    let mut service = Service::new(config).await;
+    for (key, value) in cli::zkey_config() {
+        config
+            .insert_json5(
+                &key,
+                &serde_json5::to_string(&value).unwrap_or_else(|error| {
+                    panic!("Failed to convert key value to json {key}: {error}")
+                }),
+            )
+            .unwrap_or_else(|error| panic!("Failed to insert {key}: {error}"));
+    }
+
+    let mut service = Service::new(config, cli::recorder_path()).await;
     service.run().await;
 
     Ok(())
